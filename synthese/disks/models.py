5cc5f722435eab181db7a846c08db71162e3b89a
Axelle Cousinou <axelle.cousinou@gmail.com>
2021-05-12 14:12:02 +0200

    Creation des modeles

--- synthese/disks/models.py
+++ synthese/disks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+class Track(models.Model):
+    name = models.CharField(max_length=200)
+    milliseconds = models.IntegerField()
+    bytes = models.IntegerField()
+    unitPrice = models.DecimalField(max_digits=None, decimal_places=2)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    composer = models.CharField(max_length=200)
