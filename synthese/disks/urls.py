4f0cc17c196045c64d0e1d681544eebaad147328
Axelle Cousinou <axelle.cousinou@gmail.com>
2021-05-13 18:32:42 +0200

    Tout ce qui est obligatoire est implemente

--- synthese/disks/urls.py
+++ synthese/disks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accueil)
+    path('', views.accueil),
+    path('list-tracks/<int:album_id>', views.list_tracks, name="list_tracks"),
 ]
--- synthese/disks/views.py
+++ synthese/disks/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from .models import Album
+from .models import Album, Track
+from .forms import SearchForm
 
 
 def accueil(request):
-    return render(request, 'disks/accueil.html', {'albums': Album.objects.all()})
+    valid = False
+    form = SearchForm(request.POST or None)
+    list = []
+    if form.is_valid():
+        text = form.cleaned_data['text']
+        if text=="":
+            list = Album.objects.all()
+        else:
+            list = Album.objects.filter(title__contains=text)
+        valid = True
+        return render(request, 'disks/accueil.html', {'albums': list, 'valid': valid, 'form': form})
+    return render(request, 'disks/accueil.html', {'albums': Album.objects.all(), 'valid': valid, 'form': form})
+
+
+def list_tracks(request, album_id):
+    list = Track.objects.filter(album_id=album_id)
+    return render(request, 'disks/list_tracks.html', {'album_tracks': list, 'album_name':
+        Album.objects.get(id=album_id).title, 'artist': Album.objects.get(id=album_id).artist.name})
+
